3f9de2e65cd992424b3125debdfa5b73bdce7d5c
juanibero <jibero@udesa.edu.ar>
2023-11-14 14:34:24 -0300

    cambios js

---
+++ JS/resultadobusq.js
@@ -0,0 +1,11 @@
+
+
+function guardarRespuesta() {
+  let campoBusqueda = document.getElementById("campoBusqueda");
+  a = campoBusqueda.value; // Guarda el valor del campo de búsqueda en la variable "a"
+  console.log("Respuesta del usuario: " + a);
+  return true; // Permite que el formulario se envíe
+}
+let  a = ''; // Declaración de la variable "a"
+
+let texto = document.querySelector("p").innerText = `Resultados de búsqueda para: ${a}`  
