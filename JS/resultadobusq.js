3c46d5828bc7cfb53100b7f4e8acdaeb908aac73
juanibero <jibero@udesa.edu.ar>
2023-11-14 15:09:04 -0300

    cambios js

--- JS/resultadobusq.js
+++ JS/resultadobusq.js
@@ -1,4 +1,22 @@
 
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTliN2EzYmFkNGJkNjAzZDc4MzdmYmZiZDU0NzExMyIsInN1YiI6IjY1NTI2ZjU3ZWE4NGM3MTA5NGZlYjBhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tTj2sgP2sxnhF1hIO1mv6rLXqUhxFQ_C-snrFDui8TM'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/search/multi?query=breaking&include_adult=true&language=es-AR&page=1', options)
+   .then(function(response){
+    return response.json()
+   })
+   .then(function(data){
+    console.log(data)
+   })
+   .catch(function(error){
+    console.log(error)
+   });
 
 function guardarRespuesta() {
   let campoBusqueda = document.getElementById("campoBusqueda");
@@ -9,3 +27,4 @@ function guardarRespuesta() {
 let  a = ''; // Declaración de la variable "a"
 
 let texto = document.querySelector("p").innerText = `Resultados de búsqueda para: ${a}`  
+let descripcion = document.querySelector(".texto_busqueda").innerText = `jaja  ${data.results[0]}`
