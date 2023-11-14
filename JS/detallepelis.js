c63f3714bad934f4732d3a6ddd90bc2dc8efc800
Gianbogni <gbogni@udesa.edu.ar>
2023-11-14 16:00:26 -0300

    detalle peliso

--- JS/detallepelis.js
+++ JS/detallepelis.js
@@ -6,13 +6,26 @@ const options = {
     }
   };
   
-  fetch('https://api.themoviedb.org/3/movie/movie_id?language=en-US', options)
-    .then(response => response.json())
+  fetch('https://api.themoviedb.org/3/movie/500?language=en-US', options)
+  .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
-
 let mainCont= document.querySelector(".contenedor_g12");
 
-    mainCont.innerHTML='<h1>hola</h1>'
-    
+    mainCont.innerHTML=` <div class="g_contenedor">
+    <img src="img/gladiador.jpg" alt="gladiator" class="gladiador_g">
+    <h4 class="duracion">2h 35m</h4>
+    <div>
+        <h2 class="titulo_g">Gladiator</h2>
+    <h4 class="año">(2000)</h4>
+    </div>
+ </div>
+ 
+ <div class="g2_contenedor">
+    <h3 class="titulo_g2">Accion/Aventura</h3>
+    <h4 class="estrellas_g2">⭐⭐⭐⭐⭐</h4>
+    <p class="p_g2">Máximo, general romano, desea volver a casa, pero el emperador Marco Aurelio quiere que herede el imperio. Esto hace que Cómodo ordene matar a su familia. Máximo escapa de la muerte y regresa a Roma como gladiador para vengar la muerte de su familia.</p>
+    <iframe class="trailer" width="500" height="300" src="https://www.youtube.com/embed/uvbavW31adA?si=d79XZbKHGOSUcqgy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+
+    fetch
