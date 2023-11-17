84064aecebca59b3ea63addea3cf749bcfc65aba
juanibero <jibero@udesa.edu.ar>
2023-11-17 14:42:42 -0300

    footer

--- JS/resultadobusq.js
+++ JS/resultadobusq.js
@@ -6,6 +6,12 @@ const options = {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1ZTliN2EzYmFkNGJkNjAzZDc4MzdmYmZiZDU0NzExMyIsInN1YiI6IjY1NTI2ZjU3ZWE4NGM3MTA5NGZlYjBhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tTj2sgP2sxnhF1hIO1mv6rLXqUhxFQ_C-snrFDui8TM'
     }
   };
+let http = "https://";
+let contenedorPelisPrimero = document.querySelector(".contenedor2");
+let contenedorSeriesPrimero = document.querySelector(".media rocky dos");
+let contenedorPelisFavs = document.querySelector(".favoritas");
+let fecha = document.querySelector(".mini")
+
   
   fetch('https://api.themoviedb.org/3/search/multi?query=breaking&include_adult=true&language=es-AR&page=1', options)
    .then(function(response){
@@ -13,10 +19,19 @@ const options = {
    })
    .then(function(data){
     console.log(data)
+   
+        for(let i=0; i < 3; i++){
+            fecha.innerText = data.results[i].release_date;
+    }
+    
    })
    .catch(function(error){
     console.log(error)
    });
+   
+
+
+   
 
 function guardarRespuesta() {
   let campoBusqueda = document.getElementById("campoBusqueda");
@@ -27,4 +42,4 @@ function guardarRespuesta() {
 let  a = ''; // Declaración de la variable "a"
 
 let texto = document.querySelector("p").innerText = `Resultados de búsqueda para: ${a}`  
-let descripcion = document.querySelector(".texto_busqueda").innerText = `jaja  ${data.results[0]}`
+let descripcion = document.querySelector(".texto_busqueda").innerText = `jaja  ${datas.results[0].overview}`
