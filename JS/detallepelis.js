868429b907fddd2e2612e0502caa3ded46aaba3b
Gianbogni <gbogni@udesa.edu.ar>
2023-11-17 15:44:48 -0300

    delatte pelso

--- JS/detallepelis.js
+++ JS/detallepelis.js
@@ -6,9 +6,22 @@ const options = {
     }
   };
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjU5MWY4NDM4MGM1ZjNmMTliMjc0NDhhNWU2ZjFiMCIsInN1YiI6IjY1NGU0MjE4MjkzODM1NDNmNDg1OGIxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tqMN7OL3YrQsOlckKOBcg4C39IECdKrLojZnfJxrVPw'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/movie/movie_id/reviews?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
 
   let mainCont= document.querySelector(".contenedor_g12");
-  
+  let recomendaciones= document.querySelector(".extras");
   fetch('https://api.themoviedb.org/3/movie/500?language=en-US', options)
   .then(function(response){
     return response.json()
@@ -52,6 +65,11 @@ const options = {
      <p class="p_g2">${pelis.overview}</p>`
   })
   .catch()
+
+
+
+
+
 
 
 
