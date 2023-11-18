c644f86c04d22883e100153b88b207e053215f0a
juanibero <jibero@udesa.edu.ar>
2023-11-17 23:37:14 -0300

    resulbusq

--- JS/resultadobusq.js
+++ JS/resultadobusq.js
@@ -10,39 +10,60 @@ const options = {
   let qstObj = new URLSearchParams(qst);
   let resulBusqueda = qstObj.get("buscar");
   
-  
+  let url = `https://api.themoviedb.org/3/search/multi?query=${resulBusqueda}&include_adult=true&language=es-AR&page=1`
+
   let textoBusqueda = document.querySelector(".busq");
   textoBusqueda.innerText =  `Resultado de búsqueda: ${resulBusqueda}`;
-
+ 
+  let http = "https://";
+  let contenedorPelisPrimero = document.querySelector(".contpelis2");
+  let rating = function(id){
+    if (id >=7){
+        return "green"
+    }
+    else if (id > 5 && id < 7){
+        return "yellow"
+    }
+    else{
+        return "red"
+    };
+};
   
-  fetch('https://api.themoviedb.org/3/search/multi?query=breaking&include_adult=true&language=es-AR&page=1', options)
+  fetch(url, options)
    .then(function(response){
-    return response.json()
+    return response.json();
    })
    .then(function(data){
     console.log(data)
-   
-        for(let i=0; i < 3; i++){
-           let a = fecha.innerText = data.results[i].release_date;
-           console.log(a)
-    }
+    let resultados = data.results;
+    if (resulBusqueda.length === 0) {
+      contenedorPelisPrimero.innerHTML = '<p>No hay resultado para su búsqueda</p>';
+  } 
+  else{
+    for (let i = 0; i<3; i++){
+      let api_id = resultados[i].id;
+      let peli_titulo = resultados[i].title;
+      let fecha = resultados[i].release_date;
+      let imagen = resultados[i].poster_path;
+      let resumen = resultados[i].overview
+      contenedorPelisPrimero.innerHTML += `<article class="media ${api_id}"> <img src="${http + "image.tmdb.org/t/p/w500" + resultados[i].poster_path}" alt="${resultados[i].title}"> <p class="title">${peli_titulo}</p> <p class="mini">${fecha}</p> <p class="mini">Rating: <span style= "color:${rating(Math.round(resultados[i].vote_average))}; margin-left: 5px">${Math.round(resultados[i].vote_average)}</span></p></article>`;
+  }
+
     
-   })
+    }
+      
+
+    }
+    )
+    
+
+    
+    
+      
+           
+    
    .catch(function(error){
     console.log(error)
    });
    
-
-
    
-
-function guardarRespuesta() {
-  let campoBusqueda = document.getElementById("campoBusqueda");
-  a = campoBusqueda.value; // Guarda el valor del campo de búsqueda en la variable "a"
-  console.log("Respuesta del usuario: " + a);
-  return true; // Permite que el formulario se envíe
-}
-let  a = ''; // Declaración de la variable "a"
-
-let texto = document.querySelector("p").innerText = `Resultados de búsqueda para: ${a}`  
-let descripcion = document.querySelector(".texto_busqueda").innerText = `jaja  ${datas.results[0].overview}`
