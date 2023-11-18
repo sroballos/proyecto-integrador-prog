a11aa24ddd94f1b0b7d5368eeb55f5d448af1ee6
Gianbogni <gbogni@udesa.edu.ar>
2023-11-18 00:03:35 -0300

    detalles genero

--- JS/detallegeneros.js
+++ JS/detallegeneros.js
@@ -2,35 +2,29 @@ let qst = location.search;
 let qstObj = new URLSearchParams(qst);
 let resulBusqueda = qstObj.get("buscar");
 
-let url=`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&with_genres=drama`;
-let url = `https://api.themoviedb.org/3/movie/500?language=es-AR`;
+
 const options = {
     method: 'GET',
     headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZjU5MWY4NDM4MGM1ZjNmMTliMjc0NDhhNWU2ZjFiMCIsInN1YiI6IjY1NGU0MjE4MjkzODM1NDNmNDg1OGIxZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tqMN7OL3YrQsOlckKOBcg4C39IECdKrLojZnfJxrVPw'
+      acceptar: 'application/json',
+      Authorization: 'Bearer 2f591f84380c5f3f19b27448a5e6f1b0'
     }
   };
   
   
+  
+  fetch(urlRecomendaciones, options)
 
-fetch(url, options)
+  .then(function(response){
+      return response.json();
+  })
 
-    .then(function(response){
-        return response.json();
-    })
+  .then(function(data){
+      let peliculas = data.results;
+      for (let i=0; i< 5; i++){
+        contRecom.innerHTML += `<a href = "./detallespeli.html?id=${peliculas[i].id}"><article class="media ${peliculas[i].id}"> <img src="https://${"image.tmdb.org/t/p/w500" + peliculas[i].poster_path}" alt="${peliculas[i].original_title}"> <p class="title">${peliculas[i].original_title}</p> <p class="mini">${peliculas[i].release_date}</p> <p class="mini">Rating: <span style= "color:${rating(Math.round(peliculas[i].vote_average))}; margin-left: 5px">${Math.round(peliculas[i].vote_average)}</span></p></article></a>`;
+      }
+      
+  })
+  
 
-    .then(function(data){
-        let generos=data 
-        console.log(data)
-
-
-
-
-    })
-
-
-
-    .catch(function(error){
-        console.log(error);
-    });;
